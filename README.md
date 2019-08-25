653cec2c36b6875dcf4568cf9e5abee48eaf8ba6
samuelokellogum <oksam21@gmail.com>
2019-08-25 22:15:38 +0300

    load location address

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# mapboxapi
+# Mapboxapi
 A map of resourceful places to check out while in Jinja City. This includes great restaurants, hotels, tourist attractions among others.
 
 There are also hospitals, schools and churches to look out for while in the great historic city.
+
+#How To Install
+Running locally, Install Chrome, Mozilla Firefox or Explorer  and run the index.html file.
+
+# ChinguPrework
+
+Project is live at https://s.netlify.com/
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let places = [{
         Name: "Jinja Hospital",
         Type: "Hospital",
         Address: "Rotary Rd",
-        Location: [0.4301, 33.2053]
+        Location: [0.430089, 33.205280]
 
     },
     {
@@ -24,88 +24,82 @@ let places = [{
         Name: "International Medical Centre",
         Type: "Hospital",
         Address: "Kampala Rd",
-        Location: [0.441590, 33.213200]
+        Location: [0.432213, 33.208347]
 
     },
     {
         Name: "Alshafa Medical Center",
         Type: "Hospital",
-        Address: "Mpumudde",
-        Location: [0.4271, 33.2109]
+        Address: "Mpumudde, Miro Mbili",
+        Location: [0.458519, 33.217664]
 
-    },
-    {
-        Name: "Family Hope Clinic",
-        Type: "Hospital",
-        Address: "Baxi Road",
-        Location: [0.4314402, 33.2030502]
     },
     {
         Name: "All Friends Grill Bar and Restaurant",
         Type: "Restaurants",
-        Address: "Rotary Rd",
+        Address: "Plot 6 Jackson Ave,",
         Location: [0.435643, 33.199625]
 
     },
     {
         Name: "Java House - Jinja",
         Type: "Restaurants",
-        Address: "Walukuba Masese Rd",
-        Location: [0.441590, 33.213200]
+        Address: "Shell Service Station Jinja-Kampala Highway,",
+        Location: [0.444675, 33.198979]
 
     },
     {
         Name: "Igar Cafe",
         Type: "Restaurants",
-        Address: "Madhvani Rd, Plot 32",
-        Location: [0.441590, 33.213200]
+        Address: "Main St, Jinja",
+        Location: [0.445319, 33.196919]
 
     },
     {
         Name: "Rumours At The Source",
         Type: "Restaurants",
-        Address: "Nizam Road, Jinja Town",
-        Location: [0.425830, 33.208064]
+        Address: "The Jinja Club, Nile Cres, Jinja",
+        Location: [0.32358400000000004, 32.5935104]
 
     },
     {
         Name: "Jinja Sailing Club",
         Type: "Restaurants",
-        Address: "Bugembe",
-        Location: [0.441590, 33.213200]
+        Address: "Jinja Sailing Club",
+        Location: [0.41507974999999997, 33.20475585151227]
     },
     {
         Name: "Horizon International School",
         Type: "School",
         Address: "Walukuba Masese Rd",
-        Location: [0.434820, 33.206400]
+        Location: [0.439554, 33.219560]
     },
 
     {
         Name: "Victoria Nile School",
         Type: "School",
-        Address: "Walukuba Masese Rd",
-        Location: [0.434820, 33.206402]
+        Address: "Nile Avenue, P.O.Box 501, Jinja, Uganda",
+        Location: [0.427621, 33.201745]
 
     },
     {
         Name: "Main Street Primary School",
         Type: "School",
-        Address: "Madhvani Rd, Plot 32",
+        Address: "Queen Elizabeth Way, Jinja",
         Location: [0.429530, 33.209950]
 
     },
     {
         Name: "Wanyange Girls School",
         Type: "School",
-        Address: "Nizam Road, Jinja Town",
-        Location: [0.434820, 33.206400]
+        Address: "Nyange hill",
+        Location: [0.483302, 33.245062]
     },
     {
         Name: "Busoga College Mwiri",
         Type: "School",
-        Address: "Bugembe",
-        Location: [0.421100, 33.204100]
+        Address: "Kakira Town Council, Uganda",
+        Location: [0.498532, 33.264036]
     },
     {
         Name: "St James Church of Uganda",
@@ -123,79 +117,80 @@ let places = [{
     {
         Name: "Jinja Full Gospel Church",
         Type: "Church",
-        Address: "Madhvani Rd, Plot 32",
-        Location: [0.4, 33]
+        Address: "Jinja - Iganga Highway",
+        Location: [0.446527, 33.217853]
 
     },
     {
         Name: "Deliverance Church Walukuba",
         Type: "Church",
-        Address: "Nizam Road, Jinja Town",
+        Address: "Walukuba, Jinja",
         Location: [0.432700, 33.226040]
 
     },
     {
-        Name: "Pentecostal Church",
+        Name: "Jinja Pentecostal Church",
         Type: "Church",
-        Address: "Bugembe",
-        Location: [0.441590, 33.213200]
+        Address: "Jinja - Iganga Highway",
+        Location: [0.446012, 33.217692]
 
     },
     {
         Name: "Jinja Christian Center",
         Type: "Church",
-        Address: "Bugembe",
-        Location: [0.441590, 33.213200]
+        Address: " Miiro Road, Jinja",
+        Location: [0.437937, 33.217176]
     },
     {
         Name: "St Andrews Church",
         Type: "Church",
-        Address: "Bugembe",
+        Address: "Plot10 Circular Road, P.O.Box 70",
         Location: [0.4195, 33.2065]
     },
 
     {
         Name: "Nile Village Hotel & Spa",
         Type: "Hotel",
-        Address: "Walukuba Masese Rd",
+        Address: "48/49 Kiira Road, Jinja",
         Location: [0.421800, 33.202120]
     },
     {
         Name: "Jinja Nile Resort",
         Type: "Hotel",
-        Address: "Walukuba Masese Rd",
-        Location: [0.421800, 33.202120]
+        Address: "Kimaka Road",
+        Location: [0.449829, 33.188438]
     },
     {
         Name: "Speke Courts Hotel",
         Type: "Hotel",
-        Address: "Madhvani Rd, Plot 32",
+        Address: "Plot 4 Speke Ave, Jinja",
         Location: [0.425270, 33.205580]
     },
     {
         Name: "Pearl On The Nile Hotel",
         Type: "Hotel",
-        Address: "Nizam Road, Jinja Town",
-        Location: [0.4, 33]
+        Address: "Plot 12A Kiira Road ",
+        Location: [0.429998, 33.197141]
     },
     {
         Name: "Signature Hotel Apartments",
         Type: "Hotel",
-        Address: "Bugembe",
-        Location: [0.4, 33]
+        Address: "12 Nalufenya Rd, Jinja",
+        Location: [0.441807, 33.199786]
     },
     {
-        Name: "Ci'Sand Suites",
+        Name: "Ci' Sand Suites",
         Type: "Hotel",
-        Address: "Bugembe",
-        Location: [0.446022, 33.197539]
+        Address: "Plot 15, 19 Spire Rd, Jinja",
+        Location: [0.425364, 33.212242]
     },
     {
         Name: "Brisk Recreation Hotel Triangle",
         Type: "Hotel",
-        Address: "Bugembe",
-        Location: [0.4, 33]
+        Address: "Plot 26 D-K Nile Crescent, Jinja",
+        Location: [0.417038, 33.202675]
     }
+
 ];
 
 let markers = [];
@@ -205,7 +200,7 @@ places.forEach(
         let popup = new mapboxgl.Popup({
                 offset: 25,
             })
-            .setHTML(`<h1>${place.Name}</h1>`)
+            .setHTML(`<h3>${place.Name}</h3><h4>${place.Type}</h4><h5>${place.Address}</h5>`)
 
         let marker = new mapboxgl.Marker()
             .setLngLat([place.Location[1], place.Location[0]])
